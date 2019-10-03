14d71b0eeca5b32b49046e074f32eb7a2ffbe9e7
Rubén M <ruben.mendez@gmail.com>
2019-10-03 10:58:44 +0200

    Distributed TextClients

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,8 +1,10 @@
 defmodule TextClient.Interact do
+  @hangman_server :hangman@caldofran
+
   alias TextClient.{State, Player}
 
   def start() do
-    Hangman.new_game() |> setup_state() |> Player.play()
+    new_game() |> setup_state() |> Player.play()
   end
 
   defp setup_state(game) do
@@ -10,5 +12,10 @@ defmodule TextClient.Interact do
       game_service: game,
       tally: Hangman.tally(game)
     }
+  end
+
+  defp new_game() do
+    Node.connect(@hangman_server)
+    :rpc.call(@hangman_server, Hangman, :new_game, [])
   end
 end
